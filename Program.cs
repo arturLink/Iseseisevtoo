b56fe80c4ab27f1906f74911d8b6ab3ba2368b3a
Opilane TTHK <opilane@tthk.ee>
2022-02-23 10:06:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Iseseisevtöö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int [7] { 10, 11, 12, 13, 14, 15, 16 };
+            //Console.WriteLine(numbers[0]);
+            int max_value = 0;
+            int min_value = 0;
+            int n1 = max_value + numbers[6];
+            int n2 = min_value + numbers[0];
+            int answer = n1 - n2;
+            Console.WriteLine(answer);
+
+        }
+    }
+}
