5f0a2403410c133452dc1778bb4dd200f740ec19
vafle <vafle@DESKTOP-K2BF7TN>
2022-03-15 23:31:19 +0200

    sds

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Iseseisevtöö
     {
         static void Main(string[] args)
         {
+
+            //TASK1
             //int[] numbers = new int [7] { 10, 11, 12, 13, 14, 15, 16 };
             //int max_value = 0;
             //int min_value = 0;
@@ -17,31 +19,103 @@ namespace Iseseisevtöö
             //int n2 = min_value + numbers[0];
             //int answer = n1 - n2;
             //Console.WriteLine(answer);
+            //TASK1
+
+            //TASK2
+            //int[] array = { -12, -99, 9, 122, 170, 50 };
+            //int count = array.Count(x => x > 0 && x < 125);
+            //Console.WriteLine(count);
+            //TASK2
 
 
-            //Random numb = new Random();
-            //int[] k = new int[5];
-            //for (int i = 0; i < 5; i++)
+            //TASK3
+            //List<string> players = new List<string>();
+            //players.Add("Johhny Boy");
+            //players.Add("Baby man");
+            //players.Add("Bunda Junior");
+            //players.Add("Bunda Senior");
+            //players.Add("Artur");
+
+            //foreach (string player in players)
             //{
-            //    k[i] = numb.Next(-3, 180);
+            //    Console.WriteLine(player);
             //}
 
+            //Random rnd = new Random();
+            //int value = rnd.Next(0, players.Count);
+            //players.RemoveAt(value);
 
-            Dictionary<int, string> humans = new Dictionary<int, string>(5);
-            humans.Add(1, "vasja");
-            humans.Add(2, "petja");
-            humans.Add(3, "andrei");
-            humans.Add(4, "nikita");
-            humans.Add(5, "alenka");
+            //foreach (string player in players)
+            //{
+            //    Console.WriteLine(player);
+            //}
 
-            Console.WriteLine("Remaining players:");
-            Console.WriteLine(humans);
+            //players.RemoveAt(value);
+            //foreach (string player in players)
+            //{
+            //    Console.WriteLine(player);
+            //}
 
-            Random rnd = new Random();
-            int murderindex = rnd.Next(humans.Count);
-            humans.Remove(murderindex);
-            Console.WriteLine("Remaining players:");
-            Console.WriteLine(humans);
+            //players.RemoveAt(value);
+            //foreach (string player in players)
+            //{
+            //    Console.WriteLine(player);
+            //}
+
+            //players.RemoveAt(value);
+            //foreach (string player in players)
+            //{
+            //    Console.WriteLine(player);
+            //}
+            //TASK3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Dictionary<int, string> humans = new Dictionary<int, string>(5);
+            //humans.Add(1, "vasja");
+            //humans.Add(2, "petja");
+            //humans.Add(3, "andrei");
+            //humans.Add(4, "nikita");
+            //humans.Add(5, "alenka");
+
+            //Console.WriteLine("Starting players:");
+
+
+            //Random rnd1 = new Random();
+            //int murderindex1 = rnd1.Next(humans.Count);
+            //humans.Remove(murderindex1);
+            //Console.WriteLine("Remaining players:");
+            //foreach (KeyValuePair<int, string> player in humans)
+            //{
+            //    Console.WriteLine("Number: {0}, Name: {1}", player.Key, player.Value);
+            //}
+
+            //Random rnd2 = new Random();
+            //int murderindex2 = rnd2.Next(humans.Count);
+            //humans.Remove(murderindex2);
+            //Console.WriteLine("Remaining players:");
+            //foreach (KeyValuePair<int, string> player in humans)
+            //{
+            //    Console.WriteLine("Number: {0}, Name: {1}", player.Key, player.Value);
         }
+
     }
 }
---
+++ functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Iseseisevtöö
+{
+    public class Functions
+    {
+        public void ShowPlayers(string[] players)
+        {
+            Console.WriteLine("Players:");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
