a2f8137dabea14ed526843dffb55c8553ecacd21
arturLink <93324246+arturLink@users.noreply.github.com>
2022-03-16 14:57:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -12,109 +12,103 @@ namespace Iseseisevtöö
         {
 
             //TASK1
-            //int[] numbers = new int [7] { 10, 11, 12, 13, 14, 15, 16 };
-            //int max_value = 0;
-            //int min_value = 0;
-            //int n1 = max_value + numbers[6];
-            //int n2 = min_value + numbers[0];
-            //int answer = n1 - n2;
-            //Console.WriteLine(answer);
+            int[] numbers = new int [7] { 10, 11, 12, 13, 14, 15, 16 };
+            int max_value = 0;
+            int min_value = 0;
+            int n1 = max_value + numbers[6];
+            int n2 = min_value + numbers[0];
+            int answer = n1 - n2;
+            Console.WriteLine(answer);
             //TASK1
 
             //TASK2
-            //int[] numbers = { -12, -99, 9, 122, 170, 50 };
-            //int count = numbers.Count(x => x > 0 && x < 125);
-            //Console.WriteLine(count);
+            int[] numbers = { -12, -99, 9, 122, 170, 50 };
+            int count = numbers.Count(x => x > 0 && x < 125);
+            Console.WriteLine(count);
             //TASK2
 
 
             //TASK3
-            //List<string> players = new List<string>();
-            //players.Add("Johhny Boy");
-            //players.Add("Baby man");
-            //players.Add("Bunda Junior");
-            //players.Add("Bunda Senior");
-            //players.Add("Artur");
+            List<string> players = new List<string>();
+            players.Add("Johhny Boy");
+            players.Add("Baby man");
+            players.Add("Bunda Junior");
+            players.Add("Bunda Senior");
+            players.Add("Artur");
 
-            //foreach (string player in players)
-            //{
-            //    Console.WriteLine(player);
-            //}
+            int counter = 0;
+            int murder = 2;
 
-            //Random rnd = new Random();
-            //int value = rnd.Next(0, players.Count);
-            //players.RemoveAt(value);
+            Console.WriteLine("Players");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
 
-            //foreach (string player in players)
-            //{
-            //    Console.WriteLine(player);
-            //}
+            Console.ReadLine();
 
-            //players.RemoveAt(value);
-            //foreach (string player in players)
-            //{
-            //    Console.WriteLine(player);
-            //}
+            foreach (string player in players)
+            {
+                counter++;
+                if (counter == 2)
+                {
+                    break;
+                }
+            }
 
-            //players.RemoveAt(value);
-            //foreach (string player in players)
-            //{
-            //    Console.WriteLine(player);
-            //}
+            if (counter == murder)
+            {
+                players.RemoveAt(counter - 1);
+            }
 
-            //players.RemoveAt(value);
-            //foreach (string player in players)
-            //{
-            //    Console.WriteLine(player);
-            //}
+            Console.WriteLine("Players");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.ReadLine();
+
+            if (counter == murder)
+            {
+                players.RemoveAt(counter - 1);
+            }
+
+            Console.WriteLine("Players");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.ReadLine();
+
+            if (counter == murder)
+            {
+                players.RemoveAt(counter - 1);
+            }
+
+            Console.WriteLine("Players");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.ReadLine();
+
+            if (counter == murder)
+            {
+                players.RemoveAt(counter - 1);
+            }
+
+            Console.WriteLine("Players");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.ReadLine();
+
             //TASK3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //Dictionary<int, string> humans = new Dictionary<int, string>(5);
-            //humans.Add(1, "vasja");
-            //humans.Add(2, "petja");
-            //humans.Add(3, "andrei");
-            //humans.Add(4, "nikita");
-            //humans.Add(5, "alenka");
-
-            //Console.WriteLine("Starting players:");
-
-
-            //Random rnd1 = new Random();
-            //int murderindex1 = rnd1.Next(humans.Count);
-            //humans.Remove(murderindex1);
-            //Console.WriteLine("Remaining players:");
-            //foreach (KeyValuePair<int, string> player in humans)
-            //{
-            //    Console.WriteLine("Number: {0}, Name: {1}", player.Key, player.Value);
-            //}
-
-            //Random rnd2 = new Random();
-            //int murderindex2 = rnd2.Next(humans.Count);
-            //humans.Remove(murderindex2);
-            //Console.WriteLine("Remaining players:");
-            //foreach (KeyValuePair<int, string> player in humans)
-            //{
-            //    Console.WriteLine("Number: {0}, Name: {1}", player.Key, player.Value);
         }
 
     }
